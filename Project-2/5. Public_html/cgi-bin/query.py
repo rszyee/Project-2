66ed561db8d00fd0c1d1e779ad8c853e942763cd
Rachel <r.s.z.yee@dundee.ac.uk>
2015-03-02 17:04:09 +0100

    Final commit

--- Project-2/5. Public_html/cgi-bin/query.py
+++ Project-2/5. Public_html/cgi-bin/query.py
@@ -1,25 +1,26 @@
 #!/usr/local/bin/python
-import cgi #description
+import cgi #enables us to use the cgi module
 import cgitb
 cgitb.enable()
+#activates a special exception handler that will display detailed reports on the Web browser if any errors occur
 
-form = cgi.FieldStorage()#description
-import MySQLdb #description
-sql="SELECT symbol, title FROM Genes where gene_id= %s" #description
-db=MySQLdb.connect(db='rszyee', user='rszyee', passwd='_8ydr_sW') #description
-cursor=db.cursor()#
-cursor.execute(sql,form[''].value) #
-result=cursor.fetchall() #
+form = cgi.FieldStorage()#using the FieldStorage class enables us to get submitted form data
+import MySQLdb #imports MySQLdb
+sql="SELECT symbol, title FROM Genes where gene_id= %s" #query gene symbol and title using SQL
+db=MySQLdb.connect(db='rszyee', user='rszyee', passwd='_8ydr_sW') #connecting to MySQLdb
+cursor=db.cursor()
+cursor.execute(sql,form[''].value) #queries the SQL database
+result=cursor.fetchall() #fetches the results from the SQL database
 result
 
-Expressionsql="SELECT DISTINCT accession FROM Sample INNER JOIN Expression On Sample.accession=Expression.accession INNER JOIN Probes ON Probes.reference=Expression.reference INNER JOIN Genes ON Genes.id=Probes.id WHERE id=%s and subset='seizure'" 
-cursor.execute(Expressionsql,form[''].value)
-Expressionresult=cursor.fetchall()
+Expressionsql="SELECT DISTINCT accession FROM Sample INNER JOIN Expression On Sample.accession=Expression.accession INNER JOIN Probes ON Probes.reference=Expression.reference INNER JOIN Genes ON Genes.id=Probes.id WHERE id=%s and subset='seizure'" #query accession from MySQLdb for subset seizure
+cursor.execute(Expressionsql,form[''].value)#queries the SQL database
+Expressionresult=cursor.fetchall()#fetches the results from the SQL database
 Expressionresult
 
-Expression1sql="SELECT DISTINCT accession FROM Sample INNER JOIN Expression On Sample.accession=Expression.accession INNER JOIN Probes ON Probes.reference=Expression.reference INNER JOIN Genes ON Genes.id=Probes.id WHERE id=%s and subset='control'" 
-cursor.execute(Expression1sql,form[''].value)
-Expression1result=cursor.fetchall()
+Expression1sql="SELECT DISTINCT accession FROM Sample INNER JOIN Expression On Sample.accession=Expression.accession INNER JOIN Probes ON Probes.reference=Expression.reference INNER JOIN Genes ON Genes.id=Probes.id WHERE id=%s and subset='control'" #query accession from MySQLdb for subset seizure
+cursor.execute(Expression1sql,form[''].value)#queries the SQL database
+Expression1result=cursor.fetchall()#fetches the results from the SQL database
 Expression1result
 
 print "Content-Type: text/html"     # HTML is following
@@ -27,7 +28,7 @@ print                               # blank line, end of headers
 print "<html><head><TITLE>CGI form</TITLE></head>"
 print "<body><H1>Form information</H1>"
 print "<b>Gene ID: </b>"
-#
+#page formatting using HTML
 
 print "<table><tr><th>Key</th><th>Value</th></tr>"
 
